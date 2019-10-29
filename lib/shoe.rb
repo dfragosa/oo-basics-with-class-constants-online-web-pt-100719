86776622fe9b17fd5408caaa218155abc4faff6b
Derric Fragosa <fderric@gmail.com>
2019-10-29 21:50:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,7 +11,7 @@ class Shoe
     # if !(BRANDS.include?(@brand))
     #   BRANDS << @brand
         @brand.each do |shoe|
-          if BRANDS::
+          if !BRANDS::@brand
       BRANDS << shoe
     end
   end
